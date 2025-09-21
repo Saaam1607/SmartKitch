1d0a7ee62daf29f3d2fafb0a3e69056d7fff75d0
Samuel Casagrande Cecchin <s.casagrandececchin@studenti.unitn.it>
2025-09-21 14:31:26 +0200

    feat: Enhance order management UI; add flying animation for dish addition, implement glass card styling, and update state handling for shopped items count.

--- client/src/app/orders/page.tsx
+++ client/src/app/orders/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import Navbar from 'react-bootstrap/Navbar';
 
@@ -20,6 +20,8 @@ import { ShoppingCart, Hamburger, UserRound, Plus, Minus, Search, Send } from 'l
 
 import CardImage from "../components/generic/card/CardImage";
 import { Form } from "react-bootstrap";
+
+import './glass.css';
 
 const defaultNewOrder: Order = {
   id: 0,
@@ -47,7 +49,12 @@ export default function LoginPage() {
 
   const [newOrder, setNewOrder] = useState<Order>(defaultNewOrder);
 
+  const [numberOfShoppedItems, setNumberOfShoppedItems] = useState<number>(0); 
+
   const { setLoading } = useLoading();
+
+  const flyingRefs = useRef<(HTMLDivElement | null)[]>([]);
+  const cart = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     if (menuSectionSelected) {
@@ -83,6 +90,13 @@ export default function LoginPage() {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    setNumberOfShoppedItems(
+      newOrder.ordered_dishes.reduce((total, dish) => total + dish.quantity, 0) +
+      newOrder.ordered_drinks.reduce((total, drink) => total + drink.quantity, 0)
+    );
+  }, [newOrder]);
+
   function addDishToOrder(dishName: string) {
     const existingDish = newOrder.ordered_dishes.find(d => d.dish_name === dishName);
     if (existingDish) {
@@ -111,6 +125,49 @@ export default function LoginPage() {
   function getDishQty(dishName: string): number {
     const dish = newOrder.ordered_dishes.find(d => d.dish_name === dishName);
     return dish ? dish.quantity : 0;
+  }
+
+  function addItem(index: number, item: Dish) {
+    if (flyingRefs.current[index]) {
+      flyItem(flyingRefs.current[index], item.imageUrl);
+      addDishToOrder(item.name);
+    }
+  }
+
+  function flyItem(flyingRef: HTMLDivElement, imageUrl: string, speed = 800) {
+    const imgRect = flyingRef.getBoundingClientRect();
+
+    // crea il clone dell'immagine
+    const img = document.createElement("img");
+    img.src = imageUrl;
+    img.style.position = "fixed";
+    img.style.top = `${imgRect.top}px`;
+    img.style.left = `${imgRect.left}px`;
+    img.style.width = `${imgRect.width}px`;
+    img.style.height = `${imgRect.height}px`;
+    img.style.borderRadius = "8px";
+    img.style.opacity = "1";
+    img.style.transition = `all ${speed / 1000}s ease`;
+    img.style.zIndex = "999";
+
+    document.body.appendChild(img);
+
+    if (cart.current) {
+      const cartRect = cart.current.getBoundingClientRect();
+
+      requestAnimationFrame(() => {
+        img.style.top = `${cartRect.top + window.scrollY}px`;
+        img.style.left = `${cartRect.left + window.scrollX}px`;
+        img.style.width = "50px"; // dimensione finale più piccola
+        img.style.height = "50px";
+        img.style.transform = "scale(0.3)";
+        img.style.opacity = "0";
+      });
+    }
+
+    setTimeout(() => {
+      img.remove();
+    }, speed);
   }
 
   return (
@@ -191,98 +248,147 @@ export default function LoginPage() {
 
 
       <div
-        className="d-flex w-100 flex-grow-1 justify-content-center gap-4 p-2">
-        {itemsToShow.map((item) => {
-          const quantity = getDishQty(item.name);
-          return (
-            <div
-              key={item.name}
-              className="d-flex flex-column gap-2"
-              style={{
-                borderRadius: '20px',
-                boxShadow: 'rgba(0, 0, 0, 0.2) 0px 2px 4px',
-                width: '15rem',
-                height: '18rem',
-              }}
-            >
-              <div className="w-100 flex-grow-1 d-flex flex-column justify-content-center align-items-center p-2">
-                <div style={{ width: '150px', height: '150px' }}>
-                  <CardImage imageUrl={item.imageUrl} />
-                </div>
-                <div className="w-100">
-                  <h5 className="m-0 p-0">{item.name}</h5>
-                  <p className="m-0">{item.description}</p>
-                  <p className="m-0">€ {item.price}</p>
-                </div>
-              </div>
-              <div
-                className="d-flex justify-content-between align-items-center w-100"
-                style={{
-                  borderBottomRightRadius: '20px',
-                  borderBottomLeftRadius: '20px',
-                  backgroundColor: quantity > 0 ? 'rgb(220, 24, 44)' : 'rgba(165, 165, 165, 1)',
-                  color: 'white',
-                  textAlign: 'center',
-                }}
-              >
-                {quantity > 0 ? (
-                  <>
-                    <Minus
-                      size={25}
-                      color={"white"}
-                      className="mx-3"
-                      style={{
-                        cursor: 'pointer',
-                      }}
-                      onClick={() => removeDishFromOrder(item.name)}
-                    />
-                    <h3 className="m-0 p-1">{quantity}</h3>
-                    <Plus
-                      size={25}
-                      color={"white"}
-                      className="mx-3"
-                      style={{
-                        cursor: 'pointer',
-                      }}
-                      onClick={() => addDishToOrder(item.name)}
-                    />
-                  </>
-                ) : (
-                  <button
-                    className="btn w-100"
-                    style={{
-                      color: 'white',
-                      backgroundColor: 'transparent',
-                      border: 'none',
-                    }}
-                    onClick={() => addDishToOrder(item.name)}
-                  >
-                    Add
-                  </button>
-                )}
-                
-              </div>
-            </div>
-          )
-        })}
-      </div>
-
-      <div
-        className="w-100 d-flex gap-3 justify-content-center"
+        className=""
         style={{
-          backgroundColor: 'rgb(220, 24, 44)',
+          position: 'relative',
+          overflow: 'hidden',
+          height: '100%',
         }}
       >
-        <div className="p-3">
-          <UserRound size={30} color={"white"} />
+        <div
+          className="d-flex flex-wrap w-100 flex-grow-1 justify-content-center gap-4 p-2"
+          style={{
+            maxHeight: '100%',
+            overflow: "auto",
+          }}
+        >
+          {itemsToShow.map((item, index) => {
+
+            const quantity = getDishQty(item.name);
+
+            return (
+              <div
+                key={item.name}
+                className="d-flex flex-column gap-2"
+                style={{
+                  borderRadius: '20px',
+                  boxShadow: 'rgba(0, 0, 0, 0.2) 0px 2px 4px',
+                  width: '15rem',
+                  minWidth: '15rem',
+                  height: '18rem',
+                  minHeight: '18rem',
+                  cursor: 'pointer',
+                }}
+              >
+                <div
+                  className="w-100 flex-grow-1 d-flex flex-column justify-content-center align-items-center p-2"
+                  onClick={() => { addItem(index, item) }}
+                >
+                  <div
+                    style={{ width: '150px', height: '150px' }}
+                    ref={(el) => {
+                      flyingRefs.current[index] = el;
+                    }}
+                  >
+                    <CardImage imageUrl={item.imageUrl} />
+                  </div>
+                  <div className="w-100">
+                    <h5 className="m-0 p-0">{item.name}</h5>
+                    <p className="m-0">{item.description}</p>
+                    <p className="m-0">€ {item.price}</p>
+                  </div>
+                </div>
+                <div
+                  className="d-flex justify-content-between align-items-center w-100"
+                  style={{
+                    borderBottomRightRadius: '20px',
+                    borderBottomLeftRadius: '20px',
+                    backgroundColor: quantity > 0 ? 'rgb(220, 24, 44)' : 'rgba(165, 165, 165, 1)',
+                    color: 'white',
+                    textAlign: 'center',
+                    height: '3rem',
+                  }}
+                >
+                  {quantity > 0 ? (
+                    <>
+                      <Minus
+                        size={25}
+                        color={"white"}
+                        className="mx-3"
+                        style={{
+                          cursor: 'pointer',
+                        }}
+                        onClick={() => removeDishFromOrder(item.name)}
+                      />
+                      <h3 className="m-0 p-1">{quantity}</h3>
+                      <Plus
+                        size={25}
+                        color={"white"}
+                        className="mx-3"
+                        style={{
+                          cursor: 'pointer',
+                        }}
+                        onClick={() => { addItem(index, item) }}
+                      />
+                    </>
+                  ) : (
+                    <button
+                      className="btn w-100"
+                      style={{
+                        color: 'white',
+                        backgroundColor: 'transparent',
+                        border: 'none',
+                      }}
+                      onClick={() => { addItem(index, item) }}
+                    >
+                      Add
+                    </button>
+                  )}
+                  
+                </div>
+              </div>
+            )
+          })}
         </div>
-        <div className="p-3">
-          <Hamburger size={30} color={"white"} />
+
+        <div
+          className="w-100 d-flex justify-content-center mb-2"
+          style={{
+            position: 'absolute',
+            bottom: 0,
+          }}
+        >
+          <div
+            className="glass-card d-flex w-100 gap-3 justify-content-center mx-5"
+            style={{
+              backgroundColor: 'rgba(220, 24, 44, 0.8)',
+              borderRadius: '20px',
+              maxWidth: '600px',
+            }}
+          >
+            <div className="p-3">
+              <UserRound size={30} color={"white"} />
+            </div>
+            <div className="p-3">
+              <Hamburger size={30} color={"white"} />
+            </div>
+            <div className="p-3" ref={cart}>
+              <ShoppingCart size={30} color={"white"}  />
+              <p
+                className="m-0 p-0 position-absolute translate-middle badge rounded-pill"
+                style={{
+                  fontSize: '1rem',
+                  opacity: numberOfShoppedItems > 0 ? 1 : 0,
+                }}
+              >
+                {numberOfShoppedItems}
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="p-3">
-          <ShoppingCart size={30} color={"white"} />
-        </div>
+
       </div>
+      
     </div>
   );
 }
